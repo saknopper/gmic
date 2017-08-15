ad5db32480ba9364af3370ba7f05e6349072fb41
David Tschumperlé <David.Tschumperle@gmail.com>
2017-08-15 17:36:01 +0200

    .

--- src/gmic_gimp_gtk.cpp
+++ src/gmic_gimp_gtk.cpp
@@ -4508,7 +4508,7 @@ void gmic_run(const gchar *name, gint nparams, const GimpParam *param,
   // Create resources directory.
   if (!gmic::init_rc()) {
     std::fprintf(cimg::output(),
-                 "\n[gmic_gimp]./error/ Unable to create resources directory 'gmicrc'.\n");
+                 "\n[gmic_gimp] Unable to create resources folder. Troubles are likely to happen!\n");
     std::fflush(cimg::output());
   }
 
